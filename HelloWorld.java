6cb85424aff6e56650b492867091f6a34a78f851
Saamiya <saamiya@Saamiyas-MacBook.local>
2026-03-11 11:11:53 +0530

    Implement UC3: Display name with default handling

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello " + name);
+        // Default name
+        String name = "World";
+
+        // Check if user provided a name
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        // Display personalized greeting
+        System.out.println("Hello " + name + "!");
     }
 }
