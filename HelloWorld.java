cfa3d50aaee9e5ca759ac9f712251eb252283e49
Saamiya <saamiya@Saamiyas-MacBook.local>
2026-04-13 10:24:04 +0530

    UC7: Use String.join() for multiple names

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        if(args.length == 0) {
+        if (args.length == 0) {
             System.out.println("Hello, World!");
-        } 
-        else {
-            String names = "";
-
-            for(String name : args) {
-                names += name + ", ";
-            }
-
-            // Remove trailing comma and space
-            names = names.substring(0, names.length() - 2);
-
+        } else {
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
-} 
+}
